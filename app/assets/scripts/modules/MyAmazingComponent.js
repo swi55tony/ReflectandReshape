d311ce1850da7838374b7c15ec619e9e3c37a04b
Antony <antony@antonydraper.com>
2020-07-28 13:33:24 +0100

    updated the large hero area

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyAmazingComponent() {
+    return (
+        <div>
+            <h1>This is My Amazing React Component</h1>
+            <p>React is great!!!, the sky is blue, grass is green</p>
+        </div>
+    )
+}
+
+export default MyAmazingComponent
